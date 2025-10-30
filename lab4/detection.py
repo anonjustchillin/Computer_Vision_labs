370a4b03bb15faaff9de5c7ee1703bb646adeb3d
Tina Topka <anon.justch1ll1n@gmail.com>
2025-10-30 05:23:51 +0200

    lab4 task1 done

--- lab4/detection.py
+++ lab4/detection.py
@@ -1,33 +1,20 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-from lab4.img_processing import process_image
-
-file_bing = './bing_img.png'
-file_atlas = './atlas_img.png'
 
 
-def show_img(img, title):
-    plt.imshow(img)
-    plt.axis('off')
-    plt.title(title)
-    plt.show()
-
-
-def harris_corners(img, site):
+def harris_corners(img):
     img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     float_img = np.float32(img_grey)
-    dest = cv2.cornerHarris(float_img, 8, 15, 0.05)
+    dest = cv2.cornerHarris(float_img, 2, 3, 0.04)
     dest = cv2.dilate(dest, None)
 
     img[dest > 0.01 * dest.max()] = [0, 0, 255]
-    img_rgb = cv2.cvtColor(img_atlas, cv2.COLOR_BGR2RGB)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    show_img(img_rgb, f'Harris Corners ({site} image)')
-    return dest
+    return img_rgb
 
 
-def sift_detector(img, file):
+def sift_detector(img):
     img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     sift = cv2.SIFT_create()
@@ -38,45 +25,131 @@ def sift_detector(img, file):
                             img)
     flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
 
-    # cv2.imwrite(file, kp_img)
-    show_img(kp_img, 'SIFT')
     return kp_img
 
 
-def shi_tomasi_detector(img, site):
+def shi_tomasi_detector(img):
     img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    corners = cv2.goodFeaturesToTrack(img_grey, 25, 0.01, 10)
+    corners = cv2.goodFeaturesToTrack(img_grey, 1200, 0.01, 20)
     corners = np.intp(corners)
 
-    for i in corners:
-        x, y = i.ravel()
-        cv2.circle(img, (x, y), 3, (0,0,255), -1)
-
-    show_img(img, f'Shi Tomasi corners ({site} image)')
+    return corners
 
 
+def flann(des1, des2):
+    # FLANN parameters
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)  # or pass empty dictionary
 
-## Atlas image processing and building detection
-img_atlas = cv2.imread(file_atlas)
-#show_img(img_atlas, "Фото з сайту livingatlas2")
-processed_atlas = process_image(img_atlas, "livingatlas2",
-                                    6, 260, 150,
-                                    200, 70, 0.8, False)
-#show_img(processed_atlas, "Оброблене фото livingatlas2")
-#img_atlas_harris = harris_corners(img_atlas, "livingatlas2")
-#img_atlas_sift = sift_detector(img_atlas, file_atlas)
-img_atlas_st = shi_tomasi_detector(img_atlas, "livingatlas2")
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    # Need to draw only good matches, so create a mask
+    matchesMask = [[0, 0] for i in range(len(matches))]
+
+    # ratio test as per Lowe's paper
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.7 * n.distance:
+            matchesMask[i] = [1, 0]
+
+    return matchesMask, matches
 
 
-## Bing image processing and building detection
-img_bing = cv2.imread(file_bing)
-#show_img(img_bing, "Фото з сайту Bing")
-processed_bing = process_image(img_bing, "Bing",
-                                   3, 300, 170,
-                                   300, 100, 1.5)
-#show_img(processed_bing, "Оброблене фото Bing")
-#img_bing_harris = harris_corners(img_bing, "Bing")
-#img_bing_sift = sift_detector(img_bing, file_bing)
-img_bing_st = shi_tomasi_detector(img_bing, "Bing")
+
+def sift_compare(img1, img2):
+    grey1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    grey2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    sift = cv2.SIFT_create()
+
+    kp1, des1 = sift.detectAndCompute(grey1, None)
+    kp2, des2 = sift.detectAndCompute(grey2, None)
+
+    matchesMask, matches = flann(des1, des2)
+
+    draw_params = dict(matchColor=(0, 255, 0),
+                       singlePointColor=(255, 0, 0),
+                       matchesMask=matchesMask,
+                       flags=cv2.DrawMatchesFlags_DEFAULT)
+
+    final_img = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
+
+    cv2.imshow('sift_feature_matching', final_img)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+    return final_img
+
+
+def harris_compare(img1, img2):
+    grey1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    grey2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    float1 = np.float32(grey1)
+    float2 = np.float32(grey2)
+
+    dest1 = cv2.cornerHarris(float1, 2, 3, 0.01)
+    dest1 = cv2.dilate(dest1, None)
+    ret1, dst_thresh1 = cv2.threshold(dest1, 0.01 * dest1.max(), 255, 0)
+    dst_thresh1 = np.uint8(dst_thresh1)
+
+    dest2 = cv2.cornerHarris(float2, 2, 3, 0.04)
+    dest2 = cv2.dilate(dest2, None)
+    ret2, dst_thresh2 = cv2.threshold(dest2, 0.01 * dest2.max(), 255, 0)
+    dst_thresh2 = np.uint8(dst_thresh2)
+
+    ret1, labels1, stats1, centroids1 = cv2.connectedComponentsWithStats(dst_thresh1)
+    ret2, labels2, stats2, centroids2 = cv2.connectedComponentsWithStats(dst_thresh2)
+
+    kps1 = [cv2.KeyPoint(x=float(c[0]), y=float(c[1]), size=3) for c in centroids1]
+    kps2 = [cv2.KeyPoint(x=float(c[0]), y=float(c[1]), size=3) for c in centroids2]
+
+    sift = cv2.SIFT_create()
+    kp1, des1 = sift.compute(grey1, kps1)
+    kp2, des2 = sift.compute(grey2, kps2)
+
+    matchesMask, matches = flann(des1, des2)
+
+    draw_params = dict(matchColor=(0, 255, 0),
+                       singlePointColor=(255, 0, 0),
+                       matchesMask=matchesMask,
+                       flags=cv2.DrawMatchesFlags_DEFAULT)
+
+    final_img = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
+    cv2.imshow('harris_feature_matching', final_img)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+    return final_img
+
+
+def shi_tomasi_compare(img1, img2):
+    grey1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    grey2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+    corners1 = cv2.goodFeaturesToTrack(grey1, 1200, 0.01, 20)
+    corners1 = np.intp(corners1)
+
+    corners2 = cv2.goodFeaturesToTrack(grey2, 1200, 0.01, 20)
+    corners2 = np.intp(corners2)
+
+    kps1 = [cv2.KeyPoint(x=float(x), y=float(y), size=3) for [[x, y]] in corners1]
+    kps2 = [cv2.KeyPoint(x=float(x), y=float(y), size=3) for [[x, y]] in corners2]
+
+    sift = cv2.SIFT_create()
+    kp1, des1 = sift.compute(grey1, kps1)
+    kp2, des2 = sift.compute(grey2, kps2)
+
+    matchesMask, matches = flann(des1, des2)
+
+    draw_params = dict(matchColor=(0, 255, 0),
+                       singlePointColor=(255, 0, 0),
+                       matchesMask=matchesMask,
+                       flags=cv2.DrawMatchesFlags_DEFAULT)
+
+    final_img = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
+    cv2.imshow('shi_tomasi_feature_matching', final_img)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
+    return final_img
 
---
+++ lab4/img_detection.py
@@ -0,0 +1,70 @@
+import cv2
+import matplotlib.pyplot as plt
+from lab4.detection import (harris_corners,
+                            sift_detector,
+                            shi_tomasi_detector,
+                            sift_compare, harris_compare, shi_tomasi_compare)
+
+
+file_bing = './bing_img.png'
+file_atlas = './atlas_img.png'
+
+
+def show_img(img, title):
+    plt.imshow(img)
+    plt.axis('off')
+    plt.title(title)
+    plt.show()
+
+# blurred = cv2.GaussianBlur(gray, (9, 9), 2)
+# show_img(blurred, "Застосування Гаусівського розмиття")
+
+## Atlas image processing and building detection
+img_atlas = cv2.imread(file_atlas)
+show_img(img_atlas, "Фото з сайту livingatlas2")
+
+atlas_harris = harris_corners(img_atlas)
+show_img(atlas_harris, "Harris corners (livingatlas2)")
+
+atlas_sift = sift_detector(img_atlas.copy())
+show_img(atlas_sift, "SIFT (livingatlas2)")
+
+atlas_shi_tomasi = shi_tomasi_detector(img_atlas.copy())
+for i in atlas_shi_tomasi:
+    x, y = i.ravel()
+    cv2.circle(img_atlas, (x, y), 3, (255, 0, 0), -1)
+show_img(img_atlas, "Shi Tomasi corners (livingatlas2)")
+
+
+## Bing image processing and building detection
+img_bing = cv2.imread(file_bing)
+show_img(img_bing, "Фото з сайту Bing")
+
+bing_harris = harris_corners(img_bing)
+show_img(bing_harris, "Harris corners (Bing)")
+
+bing_sift = sift_detector(img_bing.copy())
+show_img(bing_sift, "SIFT (Bing)")
+
+bing_shi_tomasi = shi_tomasi_detector(img_bing.copy())
+for i in bing_shi_tomasi:
+    x, y = i.ravel()
+    cv2.circle(img_bing, (x, y), 3, (255, 0, 0), -1)
+show_img(img_bing, "Shi Tomasi corners (Bing)")
+
+#####################
+img_atlas = cv2.imread(file_atlas)
+img_bing = cv2.imread(file_bing)
+
+show_img(img_atlas, "Фото з сайту livingatlas2")
+show_img(img_bing, "Фото з сайту Bing")
+
+harris_compared = harris_compare(img_atlas, img_bing)
+show_img(harris_compared, "Harris corners feature comparison")
+
+sift_compared = sift_compare(img_atlas, img_bing)
+show_img(sift_compared, "SIFT feature comparison")
+
+st_compared = shi_tomasi_compare(img_atlas, img_bing)
+show_img(st_compared, "Shi Tomasi corners feature comparison")
+
